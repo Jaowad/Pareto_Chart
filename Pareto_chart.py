03ad8eef272ab248febdab3306097db1d6a4ef8e
Md. Jaowad Hasan <43891126+Jaowad@users.noreply.github.com>
2023-10-26 16:15:30 -0700

    with random values

--- Pareto_chart.py
+++ Pareto_chart.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plt
 
 # Sample data (replace with your own)
-categories = ['Category A', 'Category B', 'Category C', 'Category D', 'Category E']
-frequency = [10, 8, 6, 4, 2]
+categories = ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I']
+Value = [10, 8, 6, 4, 2,3,5,2,6]
 
 # Calculate cumulative frequency
-cumulative_frequency = [sum(frequency[:i+1]) for i in range(len(frequency))]
+cumulative_frequency = [sum(Value[:i+1]) for i in range(len(Value))]
 
 # Create the Pareto chart
 fig, ax1 = plt.subplots()
 
 # Create the bar chart
-ax1.bar(categories, frequency, color='b')
+ax1.bar(categories, Value, color='b')
 ax1.set_xlabel('Categories')
 ax1.set_ylabel('Frequency', color='b')
 ax1.tick_params(axis='y', labelcolor='b')
